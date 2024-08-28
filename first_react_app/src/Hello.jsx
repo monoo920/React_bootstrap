1db66bb9dc506a3591cf95b5df1772b3009f560c
monoo <monooyadav@gmail.com>
2024-08-28 14:10:52 +0530

    commited

---
+++ first_react_app/src/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello(){
+    let name='hackya'
+    return <h2>
+    hii jsx!! {name}
+    </h2>
+}
+export default Hello
---
+++ first_react_app/src/MkButton.jsx
@@ -0,0 +1,6 @@
+function MkButton(){
+    return <button>
+        like it
+    </button>
+}
+export default MkButton
